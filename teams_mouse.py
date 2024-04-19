bf27d54a90ecc798b5be50b6204a91abd3bbb3f6
ilkercor <ilker.cor19@gmail.com>
2024-04-20 01:27:50 +0300

    Add feature to open a YouTube video when the program starts

--- teams_mouse.py
+++ teams_mouse.py
@@ -1,40 +1,24 @@
 import mouse
-
-
-# 1
-# mouse.click('right')
-
-# 2
-# print(mouse.get_position())
-
-# 3
-# mouse.drag(0, 0, 50, 50, absolute=True  , duration=0.1)
-# print(mouse.get_position())
-
-
-# mouse.drag(650, 650, 650, 650, absolute=True  , duration=0.1)
-
-#import mouse
 import time
 import math
+import webbrowser
 
+def play_video():
+    youtube_URL = "https://www.youtube.com/watch?v=U_KrB31T84g"
+    webbrowser.open(youtube_URL)
 
 def move_half_circle(x_center, y_center, radius=50, duration=0.0001):
-    # print(mouse.get_position())
-
     for angle in range(0, 181, 3):
         x = x_center + radius * math.cos(math.radians(-angle))
         y = y_center + radius * math.sin(math.radians(-angle))
         mouse.move(x, y, absolute=True, duration=0.001)
         time.sleep(duration)
 
-
-
 def move_heart():
     # Example usage
     print(mouse.get_position())
 
-    baslangicX = 800 # tested with 500
+    baslangicX = 800 #tested with 500
     baslangicy = 400
     radius = 50
 
@@ -44,24 +28,12 @@ def move_heart():
     print(mouse.get_position())
 
     # legs
-
     mouse.move(750, baslangicy+radius*1.5, absolute=True, duration=1)
     mouse.move(850, baslangicy, absolute=True, duration=1)
 
+play_video()
 move_heart()
 
 while True:
     time.sleep(300)
     move_heart()
-
-# for _ in range(5):
-#     print('moving..')
-#     mouse.drag(650, 650, 650, 650, absolute=True  , duration=0.1)
-
-#     time.sleep(1)
-
-
-# for i in range(400):
-#     mouse.click('left')
-
-
